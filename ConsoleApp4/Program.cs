a661210574b6921ed22603c2b7b734936ea92cf0
Dyan-x <kornel@bobak.ws>
2023-10-25 09:55:28 +0200

    commits

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -23,8 +23,27 @@ namespace ConsoleApp4
 
             Console.WriteLine($"{bolygos.Count} bolygó van a naprendszerben");
             Console.WriteLine($"Egy bolygonak a naprendszerben atlagosan {bolygos.Average(r => r.holdakszama)} holdja van");
-            Console.WriteLine($"A legnagyobb terfogatu bolygo a {bolygos.Max(r => r.terf)}");
+            //legnagyobb terfogatu bolygo
+            //Console.WriteLine($"A legnagyobb terfogatu bolygo: {bolygos.OrderByDescending(r => r.terf).First().name}");
+            Console.WriteLine($"A legnagobb terogatu bolygo a {bolygos.MaxBy(r => r.terf).name}");
 
+            //bolygo nev kereso
+            Console.Write($"Adja meg egy bolygo nevet: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine($"A {nev} bolygo {(bolygos.Find(r => r.name == nev) == null ? "nem" : "")} talalhato a listaban");
+
+
+
+            //holdak szama
+            Console.Write($"Adjon meg egy szamot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            //write a script that lists out all of the planets that have moons above the number given by the user
+            Console.WriteLine($"A {szam}-nal tobb holdal rendelkezo bolygok: ");
+            foreach (var item in bolygos.Where(r => r.holdakszama > szam))
+            {
+                Console.Write($"'{item.name}',");
+            }
             Console.ReadKey();
         }
     }
