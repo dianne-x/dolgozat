89dc0a8c5ccbff458a8a441f2b9e3ad26ef0cdba
Dyan-x <kornel@bobak.ws>
2023-10-25 09:29:32 +0200

    Add project files.

---
+++ ConsoleApp4/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Bolygo
+    {
+        public string name { get; set; }
+        public int holdakszama { get; set; }
+        public double terf { get; set; }
+
+        public Bolygo(string x)
+        {
+            string[] y = x.Split(';');
+            name = y[0];
+            holdakszama = Convert.ToInt32(y[1]);
+            terf = double.Parse(y[2]);
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bolygo> bolygos = new List<Bolygo>();
+            StreamReader sr = new StreamReader(@"..\..\src\solsys.txt");
+
+
+            while (!sr.EndOfStream)
+            {
+                bolygos.Add(new Bolygo(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"{bolygos.Count} bolygó van a naprendszerben");
+            Console.WriteLine($"Egy bolygonak a naprendszerben atlagosan {bolygos.Average(r => r.holdakszama)} holdja van");
+            Console.WriteLine($"A legnagyobb terfogatu bolygo a {bolygos.Max(r => r.terf)}");
+
+            Console.ReadKey();
+        }
+    }
+}
